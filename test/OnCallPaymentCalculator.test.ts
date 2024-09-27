e4b4f2b4583b18c97f8cd46afd87793851ff7df7
Eakan Gopalakrishnan <9505396+lonelydev@users.noreply.github.com>
2024-09-27 21:10:03 +0100

    feat(calc-multiple): add method to calculate payments for multiple users
    - so long as an array of OnCallUser objects is input, you get
    the oncall rates calculated for all.

--- test/OnCallPaymentCalculator.test.ts
+++ test/OnCallPaymentCalculator.test.ts
@@ -46,4 +46,84 @@ describe('should calculate the payment for an on call user', () => {
         const calculator = new KaluzaOnCallPaymentsCalculator(onCallRates);
         expect(calculator.calculateOnCallPayment(onCallUser)).toBe(700);
     });
+
+    test('- when person starts to be on-call from middle of Month 10am to end of that month', () => {
+        const onCallUser = {
+            id: '1',
+            name: 'John Doe',
+            onCallPeriods: [
+                {since: new Date('2024-08-28T10:00:00+01:00'), 
+                    until: new Date('2024-08-31T23:59:59+01:00')}
+            ]
+        };
+        const onCallRates: Record<Days, number> = {
+            [Days.SUNDAY]: 75,
+            [Days.MONDAY]: 50,
+            [Days.TUESDAY]: 50,
+            [Days.WEDNESDAY]: 50,
+            [Days.THURSDAY]: 50,
+            [Days.FRIDAY]: 75,
+            [Days.SATURDAY]: 75,
+        };
+        const calculator = new KaluzaOnCallPaymentsCalculator(onCallRates);
+        expect(calculator.calculateOnCallPayment(onCallUser)).toBe(250);
+    });
+
+    test('- when multiple people are on-call from start of Month 10am to end of that month', () => {
+
+        const onCallUsers = [
+            {
+                id: '1PF7DNAV',
+                name: 'YW Oncall',
+                onCallPeriods: [
+                    {since: new Date('2024-08-01T00:00:00+01:00'), 
+                        until: new Date('2024-08-06T10:00:00+01:00')},
+                    {since: new Date('2024-08-28T10:00:00+01:00'), 
+                        until: new Date('2024-09-01T00:00:00+01:00')}
+                ]
+            },
+            {
+                id: 'PGO3DTM',
+                name: 'SK Oncall',
+                onCallPeriods: [
+                    {since: new Date('2024-08-06T10:00:00+01:00'), 
+                        until: new Date('2024-08-15T10:00:00+01:00')},
+                    {since: new Date('2024-08-16T10:00:00+01:00'), 
+                        until: new Date('2024-08-21T10:00:00+01:00')}
+                ]
+            },
+            {
+                id: 'PINI77A',
+                name: 'EG Oncall',
+                onCallPeriods: [
+                    {since: new Date('2024-08-15T00:00:00+01:00'), 
+                        until: new Date('2024-08-16T10:00:00+01:00')}
+                ]
+            },
+            {
+                id: 'PJXZDBT',
+                name: 'CE Oncall',
+                onCallPeriods: [
+                    {since: new Date('2024-08-21T10:00:00+01:00'), 
+                        until: new Date('2024-08-28T10:00:00+01:00')}
+                ]
+            }
+        ];
+        const onCallRates: Record<Days, number> = {
+            [Days.SUNDAY]: 75,
+            [Days.MONDAY]: 50,
+            [Days.TUESDAY]: 50,
+            [Days.WEDNESDAY]: 50,
+            [Days.THURSDAY]: 50,
+            [Days.FRIDAY]: 75,
+            [Days.SATURDAY]: 75,
+        };
+        const calculator = new KaluzaOnCallPaymentsCalculator(onCallRates);
+        expect(calculator.calculateOnCallPayments(onCallUsers)).toStrictEqual({
+            "1PF7DNAV": 575,
+            "PGO3DTM": 850,
+            "PINI77A": 50,
+            "PJXZDBT": 425,
+        });
+    });
 });
