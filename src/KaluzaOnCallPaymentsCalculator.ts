334386b8e44ccde8f0b66a856fcd841049edd988
Eakan Gopalakrishnan <9505396+lonelydev@users.noreply.github.com>
2024-10-04 01:42:09 +0100

    feat(rid-enums): remove Days enum and use array for onCallRates
    - simplify the solution
    - additional check for on call periods

--- src/KaluzaOnCallPaymentsCalculator.ts
+++ src/KaluzaOnCallPaymentsCalculator.ts
@@ -1,5 +1,4 @@
 import { IOnCallPaymentsCalculator } from "./IOnCallPaymentsCalculator";
-import { Days } from "./Days";
 import { OnCallUser } from "./OnCallUser";
 
 function wasPersonOnCallOOH(dateToCheck: Date, onCallUntilDate: Date): boolean {
@@ -14,9 +13,9 @@ export function dateDiffInHours(until: Date, since: Date): number {
 }
 
 export class KaluzaOnCallPaymentsCalculator implements IOnCallPaymentsCalculator {
-    onCallRates: Record<Days, number>;
+    onCallRates: number[];
 
-    constructor(onCallRates: Record<Days, number>) {
+    constructor(onCallRates: number[]) {
         this.onCallRates = onCallRates;
     }
 
@@ -28,15 +27,10 @@ export class KaluzaOnCallPaymentsCalculator implements IOnCallPaymentsCalculator
         if (!onCallUser) {
             throw new Error("User undefined!");
         }
-        let onCallDays: Record<Days, number> = {
-            [Days.SUNDAY]: 0,
-            [Days.MONDAY]: 0,
-            [Days.TUESDAY]: 0,
-            [Days.WEDNESDAY]: 0,
-            [Days.THURSDAY]: 0,
-            [Days.FRIDAY]: 0,
-            [Days.SATURDAY]: 0,
+        if (!onCallUser.onCallPeriods) {
+            throw new Error("No on call periods defined!");
         }
+        let onCallDays: number[] = [0, 0, 0, 0, 0, 0, 0];
         for (let i = 0; i < onCallUser.onCallPeriods.length; i++) {
             if (onCallUser.onCallPeriods[i].since > onCallUser.onCallPeriods[i].until) {
                 throw new Error("Invalid date range!");
@@ -45,17 +39,14 @@ export class KaluzaOnCallPaymentsCalculator implements IOnCallPaymentsCalculator
             let curDate = onCallUser.onCallPeriods[i].since;
             while (curDate < onCallUser.onCallPeriods[i].until) {
                 if (wasPersonOnCallOOH(curDate, onCallUser.onCallPeriods[i].until)) {
-                    //console.log("curDate.getDay() as Days %s", curDate.getDay() as Days);
-                    onCallDays[curDate.getDay() as Days] += 1;
+                    onCallDays[curDate.getDay()] += 1;
                 }
                 curDate.setDate(curDate.getDate() + 1);
             }
         }
         let totalPayment = 0;
-        for (const day in onCallDays) {
-            // convert string day to number
-            let dayNum = +day;
-            totalPayment += onCallDays[dayNum as Days] * this.onCallRates[dayNum as Days];
+        for (let i=0; i < onCallDays.length; i++) {
+            totalPayment += onCallDays[i] * this.onCallRates[i];
         }
         return totalPayment;
     }
