6da61b34751d22cea4f5040fa2844d51c6048a82
Eakan Gopalakrishnan <9505396+lonelydev@users.noreply.github.com>
2024-10-15 18:06:49 +0100

    fix(missing-days): add tests for edge cases
    - added tests for edge cases
    - fixed cases where since starts during the day and until ends days after
    - updated README with a better intro, how to get started and what is to come
    - also updated the CLI help message to include examples
    - the script automatically does calculations in local timezone

--- src/OnCallPeriod.ts
+++ src/OnCallPeriod.ts
@@ -2,11 +2,11 @@ export class OnCallPeriod {
 
     readonly since: Date;
     readonly until: Date;
-    
+
     private _numberOfOohWeekDays: number = 0;
     private _numberOfOohWeekendDays: number = 0;
 
-    constructor (s:Date, u:Date) {
+    constructor(s: Date, u: Date) {
         this.since = new Date(s);
         this.until = new Date(u);
         this.initializeOohWeekDayAndWeekendDayCount();
@@ -22,7 +22,6 @@ export class OnCallPeriod {
                     this._numberOfOohWeekendDays++;
                 }
             }
-            
             curDate.setDate(curDate.getDate() + 1);
         }
     }
@@ -38,11 +37,51 @@ export class OnCallPeriod {
     private static isWeekDay(dayNum: number): boolean {
         return dayNum > 0 && dayNum < 5;
     }
-    
-    private static wasPersonOnCallOOH(dateToCheck: Date, onCallUntilDate: Date): boolean {
-        var dateToCheckEvening = new Date(dateToCheck);
-        dateToCheckEvening.setHours(18);
-        return (dateToCheckEvening > dateToCheck && dateToCheckEvening < onCallUntilDate)
+
+    /**
+     * Currently works on the assumption that dates don't have anything to do with timezones
+     * and anything after 6pm in whichever timezone the date is in is considered evening.
+     * @param since start of the shift
+     * @param until end of the shift
+     * @returns true if the person was on call OOH, false otherwise
+     */
+    private static wasPersonOnCallOOH(since: Date, until: Date): boolean {
+        /**
+         * if dateToCheck in the evening after 6pm and onCallUntilDate is at least 12 hours 
+         * longer than dateToCheck, then the person was on call OOH
+         */
+        return (OnCallPeriod.doesShiftSpanEveningTillNextDay(since, until) &&
+            OnCallPeriod.isShiftLongerThan6Hours(since, until));
+    }
+
+    private static doesShiftSpanEveningTillNextDay(since: Date, until: Date): boolean {
+        /**
+         * a shift could start during working hours and end after the working hours
+         * a shift could start after working hours and end after the working hours
+         */
+        let endOfWorkingHours = new Date(since);
+        endOfWorkingHours.setHours(17, 30);
+        return (endOfWorkingHours < until) && 
+            OnCallPeriod.doesShiftSpanDays(since, until);
+    }
+
+    private static isShiftLongerThan6Hours(date: Date, onCallUntilDate: Date): boolean {
+        return (onCallUntilDate.getTime() - date.getTime()) >= 6 * 60 * 60 * 1000;
+    }
+
+    private static doesShiftSpanDays(since: Date, until: Date): boolean {
+        /**
+         * if the dates are not the same, then the shift spans days
+         * this is to cover all cases, whether the shift since is end of a month
+         * and until is early in the next month.
+         */
+        return (since.getDate() !== until.getDate());
+    }
+
+    private static convertTZ(date: Date, timeZoneId: string): Date {
+        return new Date(
+            (typeof date === "string" ? new Date(date) : date)
+            .toLocaleString("en-GB", {timeZone: timeZoneId}));   
     }
 
     toString() {
