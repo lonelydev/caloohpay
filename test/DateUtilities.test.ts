6099fcd38d27341a67d7a1148c8ac10e25b7e99f
Eakan Gopalakrishnan <9505396+lonelydev@users.noreply.github.com>
2024-10-18 21:15:36 +0100

    fix(tests): use environment timezone and locale in tests

--- test/DateUtilities.test.ts
+++ test/DateUtilities.test.ts
@@ -2,11 +2,11 @@ import { convertTimezone, toLocaTzIsoStringWithOffset } from '../src/DateUtiliti
 import {describe, expect, test} from '@jest/globals';
 
 describe('DateUtilities.toLocaTzIsoStringWithOffset', () => {
+    const environmentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
     test('should convert UTC date to local ISO string with timezone offset', () => {
         const dateFromISOStringZ = new Date('2023-10-01T12:00:00Z');
         const localizedTzIsoStringWithOffset = toLocaTzIsoStringWithOffset(dateFromISOStringZ);
-
-        const environmentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
         const testDateConvertedToEnvTz = convertTimezone(dateFromISOStringZ, environmentTimezone, "en-US");
         const expectedLocalISOTime = toLocaTzIsoStringWithOffset(testDateConvertedToEnvTz);
         expect(localizedTzIsoStringWithOffset).toBe(expectedLocalISOTime);
@@ -15,8 +15,7 @@ describe('DateUtilities.toLocaTzIsoStringWithOffset', () => {
     test('should handle dates with positive timezone offsets', () => {
         const date = new Date('2023-10-01T12:00:00+02:00');
         const result = toLocaTzIsoStringWithOffset(date);
-        const localTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        const localTzDate = convertTimezone(date, localTimezone, "en-US");
+        const localTzDate = convertTimezone(date, environmentTimezone, "en-US");
         const expectedLocalISOTime = toLocaTzIsoStringWithOffset(localTzDate);
         expect(result).toBe(expectedLocalISOTime);
     });
@@ -24,8 +23,7 @@ describe('DateUtilities.toLocaTzIsoStringWithOffset', () => {
     test('should handle dates with negative timezone offsets', () => {
         const date = new Date('2023-10-01T12:00:00-05:00');
         const result = toLocaTzIsoStringWithOffset(date);
-        const localTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        const localTzDate = convertTimezone(date, localTimezone, "en-US");
+        const localTzDate = convertTimezone(date, environmentTimezone, "en-US");
         const expectedLocalISOTime = toLocaTzIsoStringWithOffset(localTzDate);
         expect(result).toBe(expectedLocalISOTime);
     });
